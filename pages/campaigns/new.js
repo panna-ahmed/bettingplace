ab50d1532df0ed4afe6397d1f38017bb21ca7dfe
Saleh Ahmed Panna <panna.ahmed09@gmail.com>
2022-02-10 11:27:36 +0600

    adding campaign fixed

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,16 +1,39 @@
 import React, { Component } from 'react';
-import { Form, Button, Input } from 'semantic-ui-react';
+import { Form, Button, Input, Message } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
+import factory from '../../ethereum/factory';
+import web3 from '../../ethereum/web3';
+
 class CampaignNew extends Component {
   state = {
-    minimumContribution: '',
+    minimumContribution: 0,
+    errorMessage: '',
+    loading: false,
+  };
+
+  onSubmit = async (event) => {
+    event.preventDefault();
+
+    this.setState({ loading: true, errorMessage: '' });
+    try {
+      const accounts = await web3.eth.requestAccounts();
+      //const accounts = await web3.eth.getAccounts();
+      await factory.methods.createCampaign(500).send({
+        from: accounts[0],
+      });
+      console.log('accounts:', accounts);
+    } catch (err) {
+      this.setState({ errorMessage: err.message });
+    }
+
+    this.setState({ loading: false });
   };
 
   render() {
     return (
       <Layout>
         <h3>Campaign New</h3>
-        <Form>
+        <Form error={!!this.state.errorMessage} onSubmit={this.onSubmit}>
           <Form.Field>
             <label>Minimum Contribution</label>
             <Input
@@ -18,12 +41,16 @@ class CampaignNew extends Component {
               labelPosition="right"
               value={this.state.minimumContribution}
               onChange={(event) =>
-                this.setState({ minimumContribution: event.target.value })
+                this.setState({
+                  minimumContribution: parseInt(event.target.value),
+                })
               }
             />
           </Form.Field>
-
-          <Button primary>Create</Button>
+          <Message error header="Oops!" content={this.state.errorMessage} />
+          <Button primary loading={this.state.loading}>
+            Create
+          </Button>
         </Form>
       </Layout>
     );
