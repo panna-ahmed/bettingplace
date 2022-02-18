653572f697a15cc38a12898e64ba15a1e047f7bc
Saleh Ahmed Panna <panna.ahmed09@gmail.com>
2022-02-18 14:58:36 +0600

    finalize project

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,23 +1,70 @@
 import React, { Component } from 'react';
 import Layout from '../../../components/Layout';
 import { Link } from '../../../routes';
-import { Button } from 'semantic-ui-react';
+import { Button, Table } from 'semantic-ui-react';
+import Campaign from '../../../ethereum/campaign';
+import RequestRow from '../../../components/RequestRow';
 
 class RequestIndex extends Component {
   static async getInitialProps(props) {
     const { address } = props.query;
-    console.log(address);
-    return { address };
+    const campaign = Campaign(address);
+    const requestsCount = await campaign.methods.getRequestsCount().call();
+    const approversCount = await campaign.methods.approversCount().call();
+
+    const requests = await Promise.all(
+      Array(requestsCount)
+        .fill()
+        .map((element, index) => {
+          return campaign.methods.requests(index).call();
+        })
+    );
+
+    return { address, requests, requestsCount, approversCount };
   }
+
+  renderRows() {
+    return this.props.requests.map((request, index) => {
+      return (
+        <RequestRow
+          key={index}
+          id={index}
+          request={request}
+          address={this.props.address}
+          approversCount={this.props.approversCount}
+        />
+      );
+    });
+  }
+
   render() {
+    const { Header, Row, HeaderCell, Body } = Table;
+
     return (
       <Layout>
         <h3>Request List</h3>
         <Link route={`/campaigns/${this.props.address}/requests/new`}>
           <a>
-            <Button primary>Add Request</Button>
+            <Button primary floated="right" style={{ marginBottom: 10 }}>
+              Add Request
+            </Button>
           </a>
         </Link>
+        <Table>
+          <Header>
+            <Row>
+              <HeaderCell>ID</HeaderCell>
+              <HeaderCell>Description</HeaderCell>
+              <HeaderCell>Amount</HeaderCell>
+              <HeaderCell>Recipient</HeaderCell>
+              <HeaderCell>Approval Count</HeaderCell>
+              <HeaderCell>Approve</HeaderCell>
+              <HeaderCell>Finalize</HeaderCell>
+            </Row>
+          </Header>
+          <Body>{this.renderRows().map((m, i))}</Body>
+        </Table>
+        <div>Found {this.props.requestsCount} requests</div>
       </Layout>
     );
   }
